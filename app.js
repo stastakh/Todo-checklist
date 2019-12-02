53e2695ba1b9193ec622ad3184763362d0489109
stastakh <stastakh@gmail.com>
2019-12-02 17:37:58 +0200

    add add todo route

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const fs = require('fs');
+const uuidv4 = require('uuid/v4');
 
 const app = express();
 const port = 3000;
+
+const Todo = require('./models/Todo');
 
 const FILE_PATH = './data/todos.json';
 
@@ -16,6 +19,28 @@ app.get('/todos', (req, res) => {
       throw err;
     }
     res.status(200).send(JSON.parse(data));
+  });
+});
+
+app.post('/todos', (req, res) => {
+  fs.readFile(FILE_PATH, 'utf8', (err, data) => {
+    if (err) {
+      throw err;
+    }
+    if (data) {
+      const todos = JSON.parse(data);
+      const newTodo = new Todo({
+        id: uuidv4(),
+        name: req.body.name,
+        description: req.body.description
+      });
+      todos.push(newTodo);
+      fs.writeFile(FILE_PATH, JSON.stringify(todos), err => {
+        if (err) {
+          throw err;
+        }
+      });
+    }
   });
 });
 
