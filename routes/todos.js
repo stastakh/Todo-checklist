2f49c45538fa3af36f8e4743103df29d1c297d17
stastakh <stastakh@gmail.com>
2019-12-02 18:38:16 +0200

    move logic to services dir

--- routes/todos.js
+++ routes/todos.js
@@ -1,126 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
-const fs = require('fs');
-const uuidv4 = require('uuid/v4');
+const {
+  getTodos,
+  addTodo,
+  removeTodo,
+  toggleTodoCompletion
+} = require('../services/todos');
 
 const authMiddleware = require('../middleware/auth');
 
-const Todo = require('../models/Todo');
-const todoSchema = require('../validation/todo');
-
-const FILE_PATH = './data/todos.json';
-
 // get all todos - GET /todos
 // public
-router.get('/', (req, res) => {
-  fs.readFile(FILE_PATH, 'utf8', (err, data) => {
-    if (err) {
-      throw err;
-    }
-    res.status(200).send(JSON.parse(data));
-  });
-});
+router.get('/', getTodos);
 
 // add todo - POST /todos
 // private
-router.post('/', authMiddleware, (req, res) => {
-  fs.readFile(FILE_PATH, 'utf8', (err, data) => {
-    if (err) {
-      throw err;
-    }
-    if (data) {
-      const todos = JSON.parse(data);
-      // Check if data is valid
-      const { error, value } = todoSchema.validate(req.body);
-      if (!error) {
-        // data is valid -> create new todo
-        const newTodo = new Todo({
-          id: uuidv4(),
-          name: value.name,
-          description: value.description
-        });
-        // add new todo to the todos array
-        todos.push(newTodo);
-        fs.writeFile(FILE_PATH, JSON.stringify(todos), err => {
-          if (err) {
-            throw err;
-          }
-          res.status(200).send(newTodo);
-        });
-      } else {
-        const err = {
-          status: 400,
-          message: error.details[0].message
-        };
-        res.status(err.status).send(err.message);
-      }
-    }
-  });
-});
+router.post('/', authMiddleware, addTodo);
 
 // remove todo - DELETE /todo/:id
 // private
-router.delete('/:id', authMiddleware, (req, res) => {
-  fs.readFile(FILE_PATH, 'utf8', (err, data) => {
-    if (err) {
-      throw err;
-    }
-    if (data) {
-      const todos = JSON.parse(data);
-      const index = todos.findIndex(todo => todo.id === req.params.id);
-      if (index >= 0) {
-        // delete todo from todos
-        console.log(todos);
-        const [deletedTodo] = todos.splice(index, 1);
-
-        fs.writeFile(FILE_PATH, JSON.stringify(todos), err => {
-          if (err) {
-            throw err;
-          }
-        });
-        console.log(deletedTodo);
-        res.status(200).send(deletedTodo);
-      } else {
-        const err = {
-          status: 400,
-          message: `Todo with this id: ${req.params.id} doesn't exist.`
-        };
-        res.status(err.status).send(err.message);
-      }
-    }
-  });
-});
+router.delete('/:id', authMiddleware, removeTodo);
 
 // done/undone todo - PATCH /todo/:id
 // private
-router.patch('/:id', authMiddleware, (req, res) => {
-  fs.readFile(FILE_PATH, 'utf8', (err, data) => {
-    if (err) {
-      throw err;
-    }
-    if (data) {
-      const todos = JSON.parse(data);
-      // check if there is todo with this id
-      const index = todos.findIndex(todo => todo.id === req.params.id);
-      if (index >= 0) {
-        // done/undone todo
-        todos[index].done = !todos[index].done;
-        fs.writeFile(FILE_PATH, JSON.stringify(todos), err => {
-          if (err) {
-            throw err;
-          }
-          res.status(200).send(todos[index]);
-        });
-      } else {
-        const err = {
-          status: 400,
-          message: `Todo with this id: ${req.params.id} doesn't exist.`
-        };
-        res.status(err.status).send(err.message);
-      }
-    }
-  });
-});
+router.patch('/:id', authMiddleware, toggleTodoCompletion);
 
 module.exports = router;
